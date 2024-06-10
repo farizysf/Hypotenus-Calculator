23f8fc1480a390883e577e9f860cb6f76370a070
Shahmin Fariz <shahminfariz9@gmail.com>
2024-06-11 06:27:21 +0800

    Add files via upload

---
+++ side.js
@@ -0,0 +1,35 @@
+let a;
+let b;
+let c;
+let flag = 0;
+
+document.getElementById("button").onclick = function () {
+  if (flag == 0) {
+    a = Number(document.getElementById("input").value);
+    console.log(a);
+    document.getElementById("label").innerHTML = "Please enter value for a/b";
+    let clearValue = document.getElementById("input");
+    if (clearValue.value != "") clearValue.value = "";
+    flag += 1;
+  } else if (flag == 1) {
+    b = Number(document.getElementById("input").value);
+    console.log(b);
+    c = Math.sqrt(Math.pow(a, 2) - Math.pow(b, 2));
+    console.log(c);
+    document.getElementById("p1").innerHTML = "The hypotenus is : " + a;
+    document.getElementById("p2").innerHTML =
+      "The value of the known side : " + b;
+    document.getElementById("p3").innerHTML =
+      "The value of the other side : " + c;
+  }
+};
+
+document.getElementById("reset").onclick = function () {
+  let clearValue = document.getElementById("input");
+  if (clearValue.value != "") clearValue.value = "";
+  document.getElementById("label").innerHTML = "Please enter value of h :";
+  document.getElementById("p1").innerHTML = "The hypotenus is : ";
+  document.getElementById("p2").innerHTML = "The value of the known side : ";
+  document.getElementById("p3").innerHTML = "The value of the other side : ";
+  flag = 0;
+};
